42883e22dc2babfe9f09d403e0020c6b446869f3
Jezryl <yamabags@gmail.com>
2021-03-08 10:51:21 -0700

    update page with a shop, collection preview and collection styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-import Homepage from './pages/homepage.component';
+import {Switch, Route} from 'react-router-dom';
+import HomePage from './pages/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
